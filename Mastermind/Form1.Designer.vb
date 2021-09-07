648a3d8fc10395178e0393b338c622698d737d3a
Staisha-N <11staisha@gmail.com>
2021-09-07 17:53:29 -0400

    Add project files.

---
+++ Mastermind/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInstruction = New System.Windows.Forms.Label()
+        Me.txtDevinette1 = New System.Windows.Forms.TextBox()
+        Me.txtDevinette2 = New System.Windows.Forms.TextBox()
+        Me.txtDevinette3 = New System.Windows.Forms.TextBox()
+        Me.btnVerifier = New System.Windows.Forms.Button()
+        Me.lblCouleur = New System.Windows.Forms.Label()
+        Me.lblEndroit = New System.Windows.Forms.Label()
+        Me.lblMessage2 = New System.Windows.Forms.Label()
+        Me.lblMessage1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblInstruction
+        '
+        Me.lblInstruction.AutoSize = True
+        Me.lblInstruction.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblInstruction.Location = New System.Drawing.Point(13, 23)
+        Me.lblInstruction.Name = "lblInstruction"
+        Me.lblInstruction.Size = New System.Drawing.Size(277, 40)
+        Me.lblInstruction.TabIndex = 0
+        Me.lblInstruction.Text = "Inscrire une couleur dans chacune des " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "cases (R, V, B ou J - en majuscules):"
+        '
+        'txtDevinette1
+        '
+        Me.txtDevinette1.Location = New System.Drawing.Point(309, 36)
+        Me.txtDevinette1.Name = "txtDevinette1"
+        Me.txtDevinette1.Size = New System.Drawing.Size(54, 27)
+        Me.txtDevinette1.TabIndex = 1
+        '
+        'txtDevinette2
+        '
+        Me.txtDevinette2.Location = New System.Drawing.Point(369, 36)
+        Me.txtDevinette2.Name = "txtDevinette2"
+        Me.txtDevinette2.Size = New System.Drawing.Size(53, 27)
+        Me.txtDevinette2.TabIndex = 2
+        '
+        'txtDevinette3
+        '
+        Me.txtDevinette3.Location = New System.Drawing.Point(428, 36)
+        Me.txtDevinette3.Name = "txtDevinette3"
+        Me.txtDevinette3.Size = New System.Drawing.Size(51, 27)
+        Me.txtDevinette3.TabIndex = 3
+        '
+        'btnVerifier
+        '
+        Me.btnVerifier.Location = New System.Drawing.Point(67, 87)
+        Me.btnVerifier.Name = "btnVerifier"
+        Me.btnVerifier.Size = New System.Drawing.Size(322, 27)
+        Me.btnVerifier.TabIndex = 4
+        Me.btnVerifier.Text = "Vérifier ma prévision"
+        Me.btnVerifier.UseVisualStyleBackColor = True
+        '
+        'lblCouleur
+        '
+        Me.lblCouleur.AutoSize = True
+        Me.lblCouleur.Location = New System.Drawing.Point(346, 136)
+        Me.lblCouleur.Name = "lblCouleur"
+        Me.lblCouleur.Size = New System.Drawing.Size(0, 20)
+        Me.lblCouleur.TabIndex = 5
+        '
+        'lblEndroit
+        '
+        Me.lblEndroit.AutoSize = True
+        Me.lblEndroit.Location = New System.Drawing.Point(294, 168)
+        Me.lblEndroit.Name = "lblEndroit"
+        Me.lblEndroit.Size = New System.Drawing.Size(0, 20)
+        Me.lblEndroit.TabIndex = 6
+        '
+        'lblMessage2
+        '
+        Me.lblMessage2.AutoSize = True
+        Me.lblMessage2.Location = New System.Drawing.Point(39, 168)
+        Me.lblMessage2.Name = "lblMessage2"
+        Me.lblMessage2.Size = New System.Drawing.Size(249, 20)
+        Me.lblMessage2.TabIndex = 7
+        Me.lblMessage2.Text = "Nombre de couleurs au bon endroit:"
+        '
+        'lblMessage1
+        '
+        Me.lblMessage1.AutoSize = True
+        Me.lblMessage1.Location = New System.Drawing.Point(39, 136)
+        Me.lblMessage1.Name = "lblMessage1"
+        Me.lblMessage1.Size = New System.Drawing.Size(301, 20)
+        Me.lblMessage1.TabIndex = 8
+        Me.lblMessage1.Text = "Nombre de couleurs devinées correctement:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(515, 279)
+        Me.Controls.Add(Me.lblMessage1)
+        Me.Controls.Add(Me.lblMessage2)
+        Me.Controls.Add(Me.lblEndroit)
+        Me.Controls.Add(Me.lblCouleur)
+        Me.Controls.Add(Me.btnVerifier)
+        Me.Controls.Add(Me.txtDevinette3)
+        Me.Controls.Add(Me.txtDevinette2)
+        Me.Controls.Add(Me.txtDevinette1)
+        Me.Controls.Add(Me.lblInstruction)
+        Me.Name = "Form1"
+        Me.Text = "Le jeu Mastermind"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblInstruction As Label
+    Friend WithEvents txtDevinette1 As TextBox
+    Friend WithEvents txtDevinette2 As TextBox
+    Friend WithEvents txtDevinette3 As TextBox
+    Friend WithEvents btnVerifier As Button
+    Friend WithEvents lblCouleur As Label
+    Friend WithEvents lblEndroit As Label
+    Friend WithEvents lblMessage2 As Label
+    Friend WithEvents lblMessage1 As Label
+End Class
---
+++ Mastermind/Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class Form1
+    '****************************************************************************************
+    '*Travail : Programme de jeu Mastermind
+    '*Programmeuse : Staisha Neville
+    '*Date : février 2021
+    '*But : Utilisation des énoncés If...Then...Else
+    '****************************************************************************************
+
+    Private Sub btnVerifier_Click(sender As Object, e As EventArgs) Handles btnVerifier.Click
+        '************************************************************************************
+        '*Procédure : btnVerifier_Click
+        '*But : Affiché le nombre de couleurs devinés correctement et combien sont dans le bon 
+        '      ordre
+        '************************************************************************************
+
+        'Partie 1 : afficher combien de couleurs sont dans le bon ordre.
+
+        'Déclaration des variables
+        Dim ordrecorrect1 As Integer
+        Dim ordrecorrect2 As Integer
+        Dim ordrecorrect3 As Integer
+
+        Dim strReponse1 As String = txtDevinette1.Text
+        Dim strReponse2 As String = txtDevinette2.Text
+        Dim strReponse3 As String = txtDevinette3.Text
+
+        'Structure décisionnelle imbriquée
+        If strReponse1 = "R" Then
+            ordrecorrect1 = 1
+
+        Else ordrecorrect1 = 0
+        End If
+
+        If strReponse2 = "J" Then
+            ordrecorrect2 = 1
+
+        Else ordrecorrect2 = 0
+        End If
+
+        If strReponse3 = "B" Then
+            ordrecorrect3 = 1
+
+        Else ordrecorrect3 = 0
+        End If
+
+        'Calcul du nombre de couleurs au bon endroit
+
+        Me.lblEndroit.Text = ordrecorrect1 + ordrecorrect2 + ordrecorrect3
+
+
+        '************************************************************************************
+        'Partie 2 : afficher combien de couleurs ont été devinés correctement
+
+        'Déclaration des variables
+        Dim couleurcorrect1 As Integer
+        Dim couleurcorrect2 As Integer
+        Dim couleurcorrect3 As Integer
+
+        'Structure décisionnelle imbriquée
+        If strReponse1 = "R" And strReponse2 = "R" And strReponse3 = "R" Then
+            couleurcorrect1 = 3
+        ElseIf strReponse1 = "R" And strReponse2 = "R" Then
+            couleurcorrect1 = 2
+        ElseIf strReponse1 = "R" And strReponse3 = "R" Then
+            couleurcorrect1 = 2
+        ElseIf strReponse2 = "R" And strReponse3 = "R" Then
+            couleurcorrect1 = 2
+        ElseIf strReponse1 = "R" Or strReponse2 = "R" Or strReponse3 = "R" Then
+            couleurcorrect1 = 1
+        End If
+
+        If strReponse1 = "J" And strReponse2 = "J" And strReponse3 = "J" Then
+            couleurcorrect2 = 3
+        ElseIf strReponse1 = "J" And strReponse2 = "J" Then
+            couleurcorrect2 = 2
+        ElseIf strReponse1 = "J" And strReponse3 = "J" Then
+            couleurcorrect2 = 2
+        ElseIf strReponse2 = "J" And strReponse3 = "J" Then
+            couleurcorrect2 = 2
+        ElseIf strReponse1 = "J" Or strReponse2 = "J" Or strReponse3 = "J" Then
+            couleurcorrect2 = 1
+        End If
+
+        If strReponse1 = "B" And strReponse2 = "B" And strReponse3 = "B" Then
+            couleurcorrect3 = 3
+        ElseIf strReponse1 = "B" And strReponse2 = "B" Then
+            couleurcorrect3 = 2
+        ElseIf strReponse1 = "B" And strReponse3 = "B" Then
+            couleurcorrect3 = 2
+        ElseIf strReponse2 = "B" And strReponse3 = "B" Then
+            couleurcorrect3 = 2
+        ElseIf strReponse1 = "B" Or strReponse2 = "B" Or strReponse3 = "B" Then
+            couleurcorrect3 = 1
+        End If
+
+        'Calcul du nombre de couleurs devinés correctement
+        Me.lblCouleur.Text = couleurcorrect1 + couleurcorrect2 + couleurcorrect3
+
+    End Sub
+End Class
